81f35c1e3ea6791f0a1796045a975a569c426d06
basiakus <basiakus@gmail.com>
2018-12-05 19:05:43 +0100

    Updates

---
+++ precache-manifest.cd03405682ee3d085845e91f631ad572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f750f9e1094a30333f1",
+    "url": "/foodOrderGhPage/static/js/runtime~main.4f750f9e.js"
+  },
+  {
+    "revision": "d0d3a648c0c307e7e930",
+    "url": "/foodOrderGhPage/static/js/main.d0d3a648.chunk.js"
+  },
+  {
+    "revision": "0373374fe7f308570a51",
+    "url": "/foodOrderGhPage/static/js/1.0373374f.chunk.js"
+  },
+  {
+    "revision": "d0d3a648c0c307e7e930",
+    "url": "/foodOrderGhPage/static/css/main.edf6dec0.chunk.css"
+  },
+  {
+    "revision": "008e9336462f6a6a4e424cd2e57a0eff",
+    "url": "/foodOrderGhPage/index.html"
+  }
+];
